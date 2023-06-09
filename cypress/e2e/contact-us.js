f546c6e84050f5ddf2357b62c4782b29b5a830ec
reneta <renkagh55@gmail.com>
2023-06-09 19:42:19 +0300

    mocha test

---
+++ cypress/e2e/contact-us.js
@@ -0,0 +1,11 @@
+describe("Test Contact Us form via WebdriverUni", ()=> {
+    it("Should be able to submit a Successful submition via Contact Us form", () =>{
+        //cypress code
+
+    })
+    it("Should not be able to submit a Successful submition via Contact Us form as all fields are required", () =>{
+        //cypress code
+
+    })
+    
+})
