e9d34fb9567bb775acf65cd1c394341711cd9281
reneta <renkagh55@gmail.com>
2023-06-15 15:18:10 +0300

    Assertions

--- cypress/e2e/webdriver-uni/contact-us.cy.js
+++ cypress/e2e/webdriver-uni/contact-us.cy.js
@@ -1,21 +1,23 @@
 ///<reference types = "Cypress"/>
 
-describe("Test Contact Us form via WebdriverUni", ()=> {
-    it("Should be able to submit a Successful submition via Contact Us form", () =>{
+describe("Test Contact Us form via WebdriverUni", () => {
+    it("Should be able to submit a Successful submition via Contact Us form", () => {
         cy.visit("http://www.webdriveruniversity.com/Contact-Us/contactus.html")
         cy.get('[name="first_name"]').type("John")
         cy.get('[name="last_name"]').type("Doe")
         cy.get('[name="email"]').type("john@email.com")
         cy.get('textarea.feedback-input').type("This is some comment.")
         cy.get('[type="submit"]').click()
+        cy.get('h1').should("have.text", "Thank You for your Message!")
     });
-    it("Should not be able to submit a Successful submition via Contact Us form as all fields are required", () =>{
+    it("Should not be able to submit a Successful submition via Contact Us form as all fields are required", () => {
         cy.visit("http://www.webdriveruniversity.com/Contact-Us/contactus.html")
         cy.get('[name="first_name"]').type("John")
         cy.get('[name="last_name"]').type("Doe")
         cy.get('textarea.feedback-input').type("This is some comment.")
-        cy.get('[type="submit"]').click()
+        cy.get('[type="submit"]').click();
+        cy.get('body').contains("Error: all fields are required")
     });
-    
+
 });
 
