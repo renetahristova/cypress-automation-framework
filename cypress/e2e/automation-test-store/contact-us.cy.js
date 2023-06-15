cbc36111fe610e2d8a1b4bbffcee81c2b0faca2a
reneta <renkagh55@gmail.com>
2023-06-15 15:18:21 +0300

    Assertions

--- cypress/e2e/automation-test-store/contact-us.cy.js
+++ cypress/e2e/automation-test-store/contact-us.cy.js
@@ -8,6 +8,8 @@ describe("Test Contact Us form via Automation Test Store", ()=> {
        cy.get('#ContactUsFrm_email').type("john@email.com");
        cy.get('#ContactUsFrm_enquiry').type("lorem ipsum");
        cy.get('.col-md-6 > .btn').click();
+
+       cy.get('.mb40 > :nth-child(3)').should("have.text", "Your enquiry has been successfully sent to the store owner!")
     });
     
 });
